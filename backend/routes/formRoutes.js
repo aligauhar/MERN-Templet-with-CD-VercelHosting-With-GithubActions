b13245a678ab050d7314e348e610ea29a80d876b
Ali Gauhar <aligauharcb@gmail.com>
2024-06-05 20:19:55 +0500

    new

--- backend/routes/formRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import send_FormValidation from "../controller/formController.js";
-
-const router = express.Router();
-
-router.post("/send", send_FormValidation);
-
-export default router;
